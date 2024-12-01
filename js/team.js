0d3130edcb68a982dffaeea2208e4e1fee0bfd0a
mpax235 <millerpgraves@gmail.com>
2024-12-01 12:50:52 -0800

    update

--- js/team.js
+++ js/team.js
@@ -1,9 +1,11 @@
 function showInfo(name) {
     const toggleDescriptions = {
         'sspruce': "Is apart of the Freakybob-team's graphics team (Is the only one 😭) He's really good and can cook for whatever he needs to make :fire:",
+        'tina': "Helps with testing the macOS versions of FreakyBrowse!"
     };
     const images = {
-        'sspruce': 'images/pfps/wish.png',
+        'sspruce': 'images/pfps/sspruce.png',
+        'tina': 'images/pfps/tina.webp'
     };
     const descriptionDiv = document.getElementById('description');
     const personImage = document.getElementById('person-image');
