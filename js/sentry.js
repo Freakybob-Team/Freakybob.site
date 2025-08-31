79412324e61d0dad6441d8b7370fd127c08b33de
wish13yt <100239673+wish13yt@users.noreply.github.com>
2025-08-31 18:16:39 -0500

    config sentry

--- js/sentry.js
+++ js/sentry.js
@@ -4,5 +4,18 @@
       // Adds request headers and IP for users, for more info visit:
       // https://docs.sentry.io/platforms/javascript/configuration/options/#sendDefaultPii
       sendDefaultPii: false,
+      // Alternatively, use `process.env.npm_package_version` for a dynamic release version
+      // if your build tool supports it.
+      release: "freakybobsitesentry@1.0.0",
+      integrations: [
+        Sentry.replayIntegration(),
+      ],
+
+      // Capture Replay for 10% of all sessions,
+      // plus for 100% of sessions with an error
+      // Learn more at
+      // https://docs.sentry.io/platforms/javascript/session-replay/configuration/#general-integration-configuration
+      replaysSessionSampleRate: 0.1,
+      replaysOnErrorSampleRate: 1.0,
     });
   };
